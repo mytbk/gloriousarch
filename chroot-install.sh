a6c91b57369a3163d2da93bc520bdb3345daaaec
Iru Cai <mytbk920423@gmail.com>
2017-04-22 23:25:58 +0800

    more packages, auto login

--- chroot-install.sh
+++ chroot-install.sh
@@ -12,9 +12,17 @@ sed -i 's/^#IgnorePkg.*$/IgnorePkg = linux linux-firmware/g' /etc/pacman.conf
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 echo "Server = ${MIRROR}" > /etc/pacman.d/mirrorlist
 
-pacman --noconfirm -Syu qemu flashrom debootstrap htop
+pacman --noconfirm -Syu --needed \
+	qemu flashrom debootstrap htop \
+	xorg xorg-apps xorg-xinit ttf-droid mate mate-extra \
+	firefox \
+	pidgin pidgin-otr
 
 mv /etc/pacman.conf.bak /etc/pacman.conf
 mv /etc/pacman.d/mirrorlist.bak /etc/pacman.d/mirrorlist
+
+useradd -m -G wheel arch
+sed -i 's/^# %wheel/%wheel/g' /etc/sudoers
+sed -i 's/root/arch/g' /etc/systemd/system/getty@tty1.service.d/autologin.conf
 
 gpgconf --homedir /etc/pacman.d/gnupg/ --kill gpg-agent
